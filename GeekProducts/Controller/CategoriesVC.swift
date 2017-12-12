683ef2eae00c38bbc08d049a27349d86284cd001
murarivarma <murarivarma@hotmail.com>
2017-12-12 14:55:13 +0530

    Created Category View and set IBOutlets

---
+++ GeekProducts/Controller/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeekProducts
+//
+//  Created by Murari Varma on 12/12/17.
+//  Copyright © 2017 Murari Varma. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GeekProducts/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  GeekProducts
+//
+//  Created by Murari Varma on 12/12/17.
+//  Copyright © 2017 Murari Varma. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
